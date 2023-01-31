dd6e1ddb697505ed45e21028d7f269febddbb408
andrew2719 <andrewblaze2719@gmail.com>
2023-01-31 11:15:50 +0530

    committed

---
+++ 2-2/merge_sort.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+
+
+// merge sort algorithm
+int swaps = 0;
+void merge(vector<int> &a, int l, int m, int r, int &count)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = a[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = a[m + 1 + j];
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = R[j];
+            j++;
+            count += 1;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        a[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        a[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int> &a, int l, int r, int &count)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(a, l, m, count);
+        mergeSort(a, m + 1, r, count);
+        merge(a, l, m, r, count);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++){
+        int x;
+        // genrate a random value in range of 10000
+        x = rand() % 100;
+        a[i] = x;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    int count = 0;
+    mergeSort(a, 0, n - 1, count);
+    cout<<endl;
+    cout << count << endl;
+    return 0;
+
+}
---
+++ tests/test.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    // input n eelements
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        q.push(x);
+    }
+
+    // read a value k - no of sub arrays to be formed
+    int k;
+    cin>>k;
+
+    // find max sum with total combinations of k elements in the queue
+    
+    
+
+    
+}
