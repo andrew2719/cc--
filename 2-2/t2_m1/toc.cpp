c3cc92e0eced1470ad8aeb786f74f2fa7431e26a
andrew2719 <andrewblaze2719@gmail.com>
2023-03-08 18:58:11 +0530

    commited

--- 2-2/t2_m1/toc.cpp
+++ 2-2/t2_m1/toc.cpp
@@ -7,7 +7,15 @@ using namespace std;
 // transitions contains the transitions of all states {A : {(A,1),(B,0),(B,5),(C,5)}, B : {(B,1),(C,5)}, C : {(C,1),(C,0)}}
 // transition table contains the final transition table {A : {0 : {A,B,C}, 1 : {A,B,C}}, B : {0 : {B,C}, 1 : {B,C}}, C : {0 : {C}, 1 : {C}}}
 
-map<char,map<int,set<int>>> final_transition_table(map<char,vector<pair<char,int>>> transitions,map<char,set<char>> epsilon_closures){
+
+// all the global declarations
+map<char,vector<pair<char,int>>> transitions_e_nfa_to_nfa;
+map<char,set<char>> epsilon_closures;
+map<char,map<int,set<int>>> transition_table_nfa;
+
+
+
+map<char,map<int,set<int>>> final_transition_table_nfa(map<char,vector<pair<char,int>>> transitions,map<char,set<char>> epsilon_closures){
     map<char,map<int,set<int>>> transition_table;
     
     for(auto it:epsilon_closures){
@@ -17,7 +25,9 @@ map<char,map<int,set<int>>> final_transition_table(map<char,vector<pair<char,int
             for(auto transition:transitions[clousers]){
 
                 if(transition.second!=5){
-                    transition_table[it.first][transition.second].insert(transition.first);
+                    for(auto epsilon_closure:epsilon_closures[transition.first]){
+                        transition_table[it.first][transition.second].insert(epsilon_closure);
+                    }
                 }
             }
         }
@@ -29,28 +39,23 @@ map<char,map<int,set<int>>> final_transition_table(map<char,vector<pair<char,int
 
 map<char,set<char>> find_epsilon_closure(map<char,vector<pair<char,int>>> transitions){
     map<char,set<char>> epsilon_states;
-    stack<pair<char,char>> st;
+    stack<char> st;
 
     for(auto state:transitions){
         for(auto transition:state.second){
 
             if(transition.second==5){
-                st.push({state.first,transition.first});
-                epsilon_states[state.first].insert(transition.first);
-                epsilon_states[state.first].insert(state.first);
-
-                while(!st.empty()){
-                    pair<char,char> trans = st.top(); // 1st time {a,b}
-                    st.pop();
-
-                    for(auto it:transitions[trans.second]){
-                        if(it.second==5){
-                            st.push({trans.second,it.first}); // 1st time {b,c} - .first is state to .second state
-                            epsilon_states[trans.second].insert(it.first);
-                            epsilon_states[trans.second].insert(trans.second);
-
-                        }
-                    }
+               st.push(transition.first); 
+            }
+        }
+        epsilon_states[state.first].insert(state.first);
+        while(!st.empty()){
+            char top = st.top();
+            st.pop();
+            epsilon_states[state.first].insert(top);
+            for(auto transition:transitions[top]){
+                if(transition.second==5){
+                    st.push(transition.first);
                 }
             }
         }
@@ -85,20 +90,48 @@ void print_epsilon_map(map<char,set<char>> epsilon_closures){
     }
 }
 
+void print_transition_table(map<char,map<int,set<int>>> transition_table){
+    cout<<"transition table:"<<endl;
+    for(auto it:transition_table){
+        cout<<it.first<<"->";
+        for(auto it2:it.second){
+            cout<<it2.first<<"->";
+            for(char it3:it2.second){
+                cout<<it3<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+}
+
+
+void E_NFA_TO_NFA(map<char,vector<pair<char,int>>> transitions){
+    print_map(transitions);
+
+    epsilon_closures = find_epsilon_closure(transitions);
+
+    print_epsilon_map(epsilon_closures);
+
+    transition_table_nfa = final_transition_table_nfa(transitions,epsilon_closures);
+
+    print_transition_table(transition_table_nfa);
+}
+
 
 int main(){
     /*int n;
     cout<<"Enter the number of states: ";
     cin>>n;*/
 
-    map<char,vector<pair<char,int>>> transitions;
+    //map<char,vector<pair<char,int>>> transitions;
     // input sysmbols 0 , 1
     // 5 for epsilon
 
     // sample transitions
-    transitions['A'] = {{'A',1},{'B',0},{'B',5},{'C',5}};
-    transitions['B'] = {{'B',1},{'C',5}};
-    transitions['C'] = {{'C',1},{'C',0}};
+    transitions_e_nfa_to_nfa['A'] = {{'A',0},{'B',5}};
+    transitions_e_nfa_to_nfa['B'] = {{'B',1},{'C',5}};
+    transitions_e_nfa_to_nfa['C'] = {{'C',1},{'C',0}};
 
 
     // input transitions
@@ -118,27 +151,10 @@ int main(){
         }
     }*/
 
-    print_map(transitions);
+    E_NFA_TO_NFA(transitions_e_nfa_to_nfa);
 
-    map<char,set<char>> epsilon_closures = find_epsilon_closure(transitions);
 
-    print_epsilon_map(epsilon_closures);
-
-    map<char,map<int,set<int>>> transition_table = final_transition_table(transitions,epsilon_closures);
-
-    cout<<"transition table:"<<endl;
-    for(auto it:transition_table){
-        cout<<it.first<<"->";
-        for(auto it2:it.second){
-            cout<<it2.first<<"->";
-            for(char it3:it2.second){
-                cout<<it3<<" ";
-            }
-            cout<<endl;
-        }
-        cout<<endl;
-    }
-
+    
    
     
 }
