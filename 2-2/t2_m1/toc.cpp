4cb694fb856103241fd20bd4ef8bbf1406fb0272
andrew2719 <andrewblaze2719@gmail.com>
2023-03-08 18:11:52 +0530

    commited

--- 2-2/t2_m1/toc.cpp
+++ 2-2/t2_m1/toc.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 // epsilon NFA to NFA
 
-map<char,set<char>> epsilon_closure(map<char,vector<pair<char,int>>> transitions){
+
+// epsilon closures contains the colusers of all states {A : {A,B,C}, B : {B,C}, C : {C}}
+// transitions contains the transitions of all states {A : {(A,1),(B,0),(B,5),(C,5)}, B : {(B,1),(C,5)}, C : {(C,1),(C,0)}}
+// transition table contains the final transition table {A : {0 : {A,B,C}, 1 : {A,B,C}}, B : {0 : {B,C}, 1 : {B,C}}, C : {0 : {C}, 1 : {C}}}
+
+map<char,map<int,set<int>>> final_transition_table(map<char,vector<pair<char,int>>> transitions,map<char,set<char>> epsilon_closures){
+    map<char,map<int,set<int>>> transition_table;
+    
+    for(auto it:epsilon_closures){
+
+        for(auto clousers:it.second){
+
+            for(auto transition:transitions[clousers]){
+
+                if(transition.second!=5){
+                    transition_table[it.first][transition.second].insert(transition.first);
+                }
+            }
+        }
+    }
+    
+    return transition_table;
+}
+
+
+map<char,set<char>> find_epsilon_closure(map<char,vector<pair<char,int>>> transitions){
     map<char,set<char>> epsilon_states;
     stack<pair<char,char>> st;
 
@@ -12,6 +37,7 @@ map<char,set<char>> epsilon_closure(map<char,vector<pair<char,int>>> transitions
             if(transition.second==5){
                 st.push({state.first,transition.first});
                 epsilon_states[state.first].insert(transition.first);
+                epsilon_states[state.first].insert(state.first);
 
                 while(!st.empty()){
                     pair<char,char> trans = st.top(); // 1st time {a,b}
@@ -21,6 +47,8 @@ map<char,set<char>> epsilon_closure(map<char,vector<pair<char,int>>> transitions
                         if(it.second==5){
                             st.push({trans.second,it.first}); // 1st time {b,c} - .first is state to .second state
                             epsilon_states[trans.second].insert(it.first);
+                            epsilon_states[trans.second].insert(trans.second);
+
                         }
                     }
                 }
@@ -28,6 +56,10 @@ map<char,set<char>> epsilon_closure(map<char,vector<pair<char,int>>> transitions
         }
     }
     
+    for(auto it:transitions){
+        epsilon_states[it.first].insert(it.first);
+    }
+
     return epsilon_states;
 }
 
@@ -42,9 +74,9 @@ void print_map(map<char,vector<pair<char,int>>> transitions){
     }
 }
 
-void print_epsilon_map(map<char,set<char>> epsilon_states){
+void print_epsilon_map(map<char,set<char>> epsilon_closures){
     cout<<"epsilon closures:"<<endl;
-    for(auto it:epsilon_states){
+    for(auto it:epsilon_closures){
         cout<<it.first<<"->";
         for(auto it2:it.second){
             cout<<it2<<" ";
@@ -88,8 +120,25 @@ int main(){
 
     print_map(transitions);
 
-    map<char,set<char>> e_c = epsilon_closure(transitions);
+    map<char,set<char>> epsilon_closures = find_epsilon_closure(transitions);
 
-    print_epsilon_map(e_c);
+    print_epsilon_map(epsilon_closures);
+
+    map<char,map<int,set<int>>> transition_table = final_transition_table(transitions,epsilon_closures);
+
+    cout<<"transition table:"<<endl;
+    for(auto it:transition_table){
+        cout<<it.first<<"->";
+        for(auto it2:it.second){
+            cout<<it2.first<<"->";
+            for(char it3:it2.second){
+                cout<<it3<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+
+   
     
 }
