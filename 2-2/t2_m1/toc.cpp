a0c1a56032c187d49697af1405853d36e23fc9d3
andrew2719 <andrewblaze2719@gmail.com>
2023-03-08 21:37:14 +0530

    cmmited

--- 2-2/t2_m1/toc.cpp
+++ 2-2/t2_m1/toc.cpp
@@ -1,17 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-// epsilon NFA to NFA
 
 
-// epsilon closures contains the colusers of all states {A : {A,B,C}, B : {B,C}, C : {C}}
-// transitions contains the transitions of all states {A : {(A,1),(B,0),(B,5),(C,5)}, B : {(B,1),(C,5)}, C : {(C,1),(C,0)}}
-// transition table contains the final transition table {A : {0 : {A,B,C}, 1 : {A,B,C}}, B : {0 : {B,C}, 1 : {B,C}}, C : {0 : {C}, 1 : {C}}}
 
 
 // all the global declarations
 map<char,vector<pair<char,int>>> transitions_e_nfa_to_nfa;
 map<char,set<char>> epsilon_closures;
 map<char,map<int,set<int>>> transition_table_nfa;
+map<string,map<int,string>> transition_table_dfa;
 
 
 
@@ -91,7 +88,7 @@ void print_epsilon_map(map<char,set<char>> epsilon_closures){
 }
 
 void print_transition_table(map<char,map<int,set<int>>> transition_table){
-    cout<<"transition table:"<<endl;
+    cout<<"transition table nfa:"<<endl;
     for(auto it:transition_table){
         cout<<it.first<<"->";
         for(auto it2:it.second){
@@ -105,6 +102,36 @@ void print_transition_table(map<char,map<int,set<int>>> transition_table){
     }
 }
 
+// epsilon closures contains the colusers of all states {A : {A,B,C}, B : {B,C}, C : {C}}
+// transitions contains the transitions of all states {A : {(A,1),(B,0),(B,5),(C,5)}, B : {(B,1),(C,5)}, C : {(C,1),(C,0)}}
+// transition table contains the final transition table {A : {0 : {B,C}, 1 : {A,B,C}}, B : ....like that
+
+map<string,map<int,string>> clearing_dups_in_dfa(map<string,map<int,string>> transition_table_dfa){
+    for(auto it:transition_table_dfa){
+        for(auto inner_map:it.second){
+            string s = "";
+            for(int i=0;i<inner_map.second.size();i++){
+                if(s.find(inner_map.second[i])==string::npos){
+                    s+=inner_map.second[i];
+                }
+            }
+            transition_table_dfa[it.first][inner_map.first] = s;
+        }
+    }
+    return transition_table_dfa;
+}
+
+void print_dfa(map<string,map<int,string>> transition_table_dfa){
+    cout<<"transition table dfa:"<<endl;
+    for(auto it:transition_table_dfa){
+        cout<<it.first<<"->";
+        for(auto it2:it.second){
+            cout<<it2.first<<"->";
+            cout<<it2.second<<endl;
+        }
+        cout<<endl;
+    }
+}
 
 void E_NFA_TO_NFA(map<char,vector<pair<char,int>>> transitions){
     print_map(transitions);
@@ -118,7 +145,48 @@ void E_NFA_TO_NFA(map<char,vector<pair<char,int>>> transitions){
     print_transition_table(transition_table_nfa);
 }
 
+void NFA_TO_DFA(map<char,map<int,set<int>>> transition_table_nfa){
 
+
+    
+    set<string> new_states;
+    for(auto it:transition_table_nfa){
+
+        for(auto inner_map:it.second){
+            string s = "";
+            for(auto inner_set:inner_map.second){
+                s+=inner_set;
+            }
+
+            stringstream ss;
+            ss<<it.first;
+            string s2 = ss.str();
+
+            transition_table_dfa[s2][inner_map.first] = s;
+            new_states.insert(s);
+        }
+    }
+
+    for(string new_state:new_states){
+        for(int i=0;i<new_state.size();i++){
+            set<char> states;
+            for(auto inner_map:transition_table_nfa[new_state[i]]){
+                for(auto inner_set:inner_map.second){
+                    states.insert(inner_set);
+                }
+                for(char state:states){
+                    transition_table_dfa[new_state][inner_map.first]+=state;
+                }
+            }
+        }
+    }
+    
+
+    transition_table_dfa = clearing_dups_in_dfa(transition_table_dfa);
+
+    print_dfa(transition_table_dfa);
+
+}
 int main(){
     /*int n;
     cout<<"Enter the number of states: ";
@@ -129,9 +197,10 @@ int main(){
     // 5 for epsilon
 
     // sample transitions
-    transitions_e_nfa_to_nfa['A'] = {{'A',0},{'B',5}};
+    transitions_e_nfa_to_nfa['A'] = {{'A',1},{'B',0},{'B',5},{'C',5}};
     transitions_e_nfa_to_nfa['B'] = {{'B',1},{'C',5}};
     transitions_e_nfa_to_nfa['C'] = {{'C',1},{'C',0}};
+    
 
 
     // input transitions
@@ -152,6 +221,8 @@ int main(){
     }*/
 
     E_NFA_TO_NFA(transitions_e_nfa_to_nfa);
+
+    NFA_TO_DFA(transition_table_nfa);
 
 
     
