e1133dff1470dc7db9fe164dcfacf1fadaf19e64
andrew2719 <andrewblaze2719@gmail.com>
2023-03-09 22:15:40 +0530

    committed

--- tests/test2.py
+++ tests/test2.py
@@ -1,70 +1,77 @@
 import random
 import time
+import statistics
+
+# quick sort function
+
+def quick_sort(arr, pivot_type):
+    if len(arr) <= 1:
+        return arr
+    if pivot_type == 0:
+        pivot = arr[0]
+    elif pivot_type == 1:
+        pivot = arr[random.randint(0, len(arr) - 1)]
+    elif pivot_type == 2:
+        r = arr[0]
+        s = arr[len(arr) // 2]
+        t = arr[len(arr) - 1]
+        pivot = statistics.median([r, s, t])
+    elif pivot_type == 3:
+        
+        r = arr[len(arr) // 4]
+        s = arr[len(arr) // 2]
+        t = arr[3 * len(arr) // 4]
+        pivot = statistics.median([r, s, t])
+    less = []
+    equal = []
+    greater = []
+    for x in arr:
+        if x < pivot:
+            less.append(x)
+        elif x == pivot:
+            equal.append(x)
+        else:
+            greater.append(x)
+    return quick_sort(less, pivot_type) + equal + quick_sort(greater, pivot_type)
+
+
+if __name__ == '__main__':
+    
+    
+    arr = [random.randint(0, 10000000) for i in range(10000000)]
+    # create 4 copies of the array
+    arr1 = arr.copy()
+    arr2 = arr.copy()
+    arr3 = arr.copy()
+    arr4 = arr.copy()
+    # print("Original array: ", arr)
+
+    
+    start_time = time.time()
+    arr = quick_sort(arr1, 0)
+    # print("Sorted array: ", arr)
+    print("Time taken: ", round(time.time() - start_time, 20))
+
+    start_time = time.time()
+    arr = quick_sort(arr2, 1)
+    # print("Sorted array: ", arr)
+    print("Time taken: ", round(time.time() - start_time, 20))
+
+    start_time = time.time()
+    arr = quick_sort(arr3, 2)
+    # print("Sorted array: ", arr)
+    print("Time taken: ", round(time.time() - start_time, 20))
+
+    start_time = time.time()
+    arr = quick_sort(arr4, 3)
+    # print("Sorted array: ", arr)
+    print("Time taken: ", round(time.time() - start_time, 20))
+
+    
+    
 
 
 
-# Function to find the partition position
-def partition(array, low, high):
-  
-    # Choose the rightmost element as pivot
-    pivot = array[high]
-  
-    # Pointer for greater element
-    i = low - 1
-  
-    # Traverse through all elements
-    # compare each element with pivot
-    for j in range(low, high):
-        if array[j] <= pivot:
-            # If element smaller than pivot is found
-            # swap it with the greater element pointed by i
-            i = i + 1
-  
-            # Swapping element at i with element at j
-            (array[i], array[j]) = (array[j], array[i])
-  
-    # Swap the pivot element with 
-    # e greater element specified by i
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-  
-    # Return the position from where partition is done
-    return i + 1
-  
-# Function to perform quicksort
-  
-  
-def quick_sort(array, low, high):
-    if low < high:
-  
-        # Find pivot element such that
-        # element smaller than pivot are on the left
-        # element greater than pivot are on the right
-        pi = partition(array, low, high)
-  
-        # Recursive call on the left of pivot
-        quick_sort(array, low, pi - 1)
-  
-        # Recursive call on the right of pivot
-        quick_sort(array, pi + 1, high)
 
 
-case = int(input("Enter the case number: "))
-if case ==1:
-    # piviot = arr[0]
-    # Generate a list of 1000 random numbers
-    arr = [random.randint(0, 50) for i in range(10)]
-    print(arr)
-    # Sort the list
-    start = time.time()
-    quick_sort(arr,0,len(arr)-1)
-    end = time.time()
-    # Print the time it took to sort the list
-    print(end - start)
-    print(arr)
-elif case ==2:
-    #piviot is the randam element from the array
-    arr = [random.randint(0, 50) for i in range(10)]
-    print(arr)
-    # Sort the list
-    start = time.time()
-    quick_sort(arr,0,len(arr)-1)
+    
