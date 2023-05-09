03a2efc4401f6fc50cf559a508a84dca1ce64059
andrew2719 <andrewblaze2719@gmail.com>
2023-05-09 20:41:16 +0530

    committed

--- 2-2/lab/backtracking/s_subst.cpp
+++ 2-2/lab/backtracking/s_subst.cpp
@@ -1,8 +1,6 @@
-
-
 #include<bits/stdc++.h>
 using namespace std;
-
+vector<vector<int>> outputs;
 void s_subst(vector<int> a,int k,int sum,int i,vector<int> &x){
     if(sum==k){
         for(int j=0;j<x.size();j++){
@@ -29,12 +27,60 @@ int main(){
     cin>>n;
     cin>>k;
     vector<int> a(n);
+    
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
+    reverse(a.begin(),a.end());
 
     vector<int> x;
     s_subst(a,k,0,0,x);
     return 0;
 
 }
+
+// #include <stdio.h>
+// int n;
+// int m;
+// int a[100];
+// int track[100];
+// int flag = 0;
+// void backtrack(int a[], int n1, int sum1)
+// {
+//     int sum = sum1, i;
+//     if (sum == m)
+//     {
+//         flag = 1;
+//         for (i = 0; i < n; i++)
+//             if (track[i] == 1)
+//                 printf("%d ", a[i]);
+//         printf("\n");
+//         return;
+//     }
+//     if (sum > m)
+//         return;
+//     for (i = n1; i >= 0; i--)
+//     {
+//         sum = sum + a[i];
+//         track[i] = 1;
+//         backtrack(a, i - 1, sum);
+//         sum = sum - a[i];
+//         track[i] = 0;
+//     }
+// }
+// int main()
+// {
+//     scanf("%d", &n);
+//     scanf("%d", &m);
+//     int i;
+//     // 6
+//     // 30
+//     // 5 10 12 13 15 18
+//     for (i = 0; i < n; i++)
+//         scanf("%d", &a[i]);
+//     for (i = 0; i < n; i++)
+//         track[i] = 0;
+//     backtrack(a, n - 1, 0);
+//     if (flag == 0)
+//         printf("No Solution");
+// }
--- 2-2/t2_m2/daa.cpp
+++ 2-2/t2_m2/daa.cpp
@@ -1,76 +1,64 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n;
-    n = 5;
-    // cout<<"Enter the number of required cut pieces: ";
-    // cin>>n;
-    int s[n] = {3,5,2,1,4};
-    //cout<<"Enter the required cut pieces: ";
-    // for(int i=0;i<n;i++)
-    // {
-    //     cin>>s[i];
+// using recursion
+int using_recursion(int n, int r1,int r2, int c1, int c2,vector<int> cut_pieces){
+    if(n==0){
+        return 0;
+    }
+    if(cut_pieces[n-1]<=r1 && cut_pieces[n-1]<=r2){
+        return min(c1,c2)+using_recursion(n-1,r1,r2,c1,c2,cut_pieces);
+    }
+    else if(cut_pieces[n-1]<=r1){
+        return c1+using_recursion(n-1,r1,r2,c1,c2,cut_pieces);
+    }
+    else if(cut_pieces[n-1]<=r2){
+        return c2+using_recursion(n-1,r1,r2,c1,c2,cut_pieces);
+    }
+    else{
+        return 0+using_recursion(n-1,r1,r2,c1,c2,cut_pieces);
+    }
+}
+
+
+
+int using_dp(int n, int r1,int r2, int c1, int c2,vector<int> cut_pieces){
+    int dp[n+1]={0};
+    for(int i=1;i<=n;i++){
+        if(cut_pieces[i-1]<=r1 && cut_pieces[i-1]<=r2){
+            dp[i] = dp[i-1]+min(c1,c2);
+        }
+        else if(cut_pieces[i-1]<=r1){
+            dp[i] = dp[i-1]+c1;
+        }
+        else if(cut_pieces[i-1]<=r2){
+            dp[i] = dp[i-1]+c2;
+        }
+        else{
+            dp[i] = dp[i-1];
+        }
+
+    }
+    return dp[n];
+    // print the dp array
+    // for(int i=0;i<=n;i++){
+    //     cout<<dp[i]<<" ";
     // }
-    int r1,r2,c1,c2;
-    r1 = 5;
-    r2 = 3;
-    c1 = 2;
-    c2 = 3;
-    // cout<<"Enter the length of rod 1: ";
-    // cin>>r1;
-    // cout<<"Enter the cost of rod 1: ";
-    // cin>>c1;
-    // cout<<"Enter the length of rod 2: ";
-    // cin>>r2;
-    // cout<<"Enter the cost of rod 2: ";
-    // cin>>c2;
-    int dp[n+1][n+1];
-    for(int i=0;i<=n;i++)
-    {
-        dp[i][0]=0;
-        dp[0][i]=0;
-    }
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            if(s[i-1]<=r1 && s[i-1]<=r2)
-            {
-                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+min(c1,c2);
-            }
-            else if(s[i-1]<=r1)
-            {
-                dp[i][j]=dp[i-1][j]+c1;
-            }
-            else if(s[i-1]<=r2)
-            {
-                cout<<"here"<<endl;
-                dp[i][j]=dp[i][j-1]+c2;
-            }
-            else
-            {
-                dp[i][j]=0;
-            }
-        }
-    }
-    
+}
 
 
-    cout<<"The minimum cost is: "<<dp[n][n]<<endl;
-
-    // printing the dp table
-
-    cout<<"The dp table is: "<<endl;
-    for(int i=0;i<=n;i++)
-    {
-        for(int j=0;j<=n;j++)
-        {
-            cout<<dp[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+int main(){
+    int rod_type1_length = 3, rod_type2_length = 5,cost_rod_1 = 2, cost_rod_2 = 3;
+    int no_of_cut_pieces;
+    //cin>>no_of_cut_pieces;
+    no_of_cut_pieces = 5;
+    vector<int> cut_pieces = {2,4,1,5,3};
+    cout<<"using dp:"<<using_dp(no_of_cut_pieces,rod_type1_length,rod_type2_length,cost_rod_1,cost_rod_2,cut_pieces);
+    cout<<"\nusing recursion :"<<using_recursion(no_of_cut_pieces,rod_type1_length,rod_type2_length,cost_rod_1,cost_rod_2,cut_pieces);
     return 0;
+
+   
+
+
+
 }
