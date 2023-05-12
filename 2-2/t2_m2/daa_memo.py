fe2dddee61cc0abd3069bf339e18f2e92879dede
andrew2719 <your-email@example.comcls>
2023-05-12 17:06:47 +0530

    committed

--- 2-2/t2_m2/daa_memo.py
+++ 2-2/t2_m2/daa_memo.py
@@ -1,6 +1,6 @@
 rods = [12, 15]
 costs = [2, 3]
-cut_pieces = [3, 5, 9, 10]
+cut_pieces = [6,6,6,6,6,6,6,6]
 cut_pieces.sort(reverse=True)
 n = len(cut_pieces)
 
@@ -9,6 +9,7 @@ n = len(cut_pieces)
 # these list will contain the remaining length of the tyope of rod after cutting the pieces
 # we traverse all the possible combinations and print  min cost and the total no of types of rods that are used
 
+min_cost = float('inf')
 
 # r1_remains = []
 # r2_remains = []
@@ -16,7 +17,7 @@ def min_cost_fun(r1_remains,r2_remains,i):
     if i == n:
         return 0
     print(r1_remains,r2_remains,i)
-    min_cost = float('inf')
+    global min_cost
     if len(r1_remains):
         for x in range(len(r1_remains)):
             if r1_remains[x]>=cut_pieces[i]:
@@ -24,7 +25,8 @@ def min_cost_fun(r1_remains,r2_remains,i):
                 new_r1_remains = []
                 new_r1_remains.append(remaining)
                 print("new_remains",new_r1_remains)
-                min_cost = min(min_cost,costs[0]+min_cost_fun(new_r1_remains,r2_remains,i+1))
+                min_cost = min(min_cost,min_cost_fun(new_r1_remains,r2_remains,i+1))
+                print("min_cost",min_cost)
 
 
 
@@ -34,18 +36,23 @@ def min_cost_fun(r1_remains,r2_remains,i):
                 remaining = r2_remains[x]-cut_pieces[i]
                 new_r2_remains = []
                 new_r2_remains.append(remaining)
-                min_cost = min(min_cost,costs[1]+min_cost_fun(r1_remains,new_r2_remains,i+1))
+                min_cost = min(min_cost,min_cost_fun(r1_remains,new_r2_remains,i+1))
+                print("min_cost",min_cost)
             
 
     if rods[0]>=cut_pieces[i]:
         r1_remains.append(rods[0]-cut_pieces[i])
         min_cost = min(min_cost,costs[0]+min_cost_fun(r1_remains,r2_remains,i+1))
         r1_remains.pop()
+        print("min_cost",min_cost)
 
     if rods[1]>=cut_pieces[i]:
         r2_remains.append(rods[1]-cut_pieces[i])
         min_cost = min(min_cost,costs[1]+min_cost_fun(r1_remains,r2_remains,i+1))
         r2_remains.pop()
+        print("min_cost",min_cost)
+
+
 
     return min_cost
 
