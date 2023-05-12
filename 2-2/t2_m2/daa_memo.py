02ce08359df69bb7578bbe42b81552fc8b6022b5
andrew2719 <your-email@example.comcls>
2023-05-12 19:45:28 +0530

    committed

--- 2-2/t2_m2/daa_memo.py
+++ 2-2/t2_m2/daa_memo.py
@@ -11,12 +11,10 @@ n = len(cut_pieces)
 
 min_cost = float('inf')
 
-# r1_remains = []
-# r2_remains = []
 def min_cost_fun(r1_remains,r2_remains,i):
     if i == n:
         return 0
-    print(r1_remains,r2_remains,i)
+    
     global min_cost
     if len(r1_remains):
         for x in range(len(r1_remains)):
@@ -24,11 +22,8 @@ def min_cost_fun(r1_remains,r2_remains,i):
                 remaining = r1_remains[x]-cut_pieces[i]
                 new_r1_remains = []
                 new_r1_remains.append(remaining)
-                print("new_remains",new_r1_remains)
                 min_cost = min(min_cost,min_cost_fun(new_r1_remains,r2_remains,i+1))
-                print("min_cost",min_cost)
-
-
+                
 
     if len(r2_remains):
         for x in range(len(r2_remains)):
@@ -37,24 +32,24 @@ def min_cost_fun(r1_remains,r2_remains,i):
                 new_r2_remains = []
                 new_r2_remains.append(remaining)
                 min_cost = min(min_cost,min_cost_fun(r1_remains,new_r2_remains,i+1))
-                print("min_cost",min_cost)
+                #print("min_cost",min_cost)
             
 
     if rods[0]>=cut_pieces[i]:
         r1_remains.append(rods[0]-cut_pieces[i])
         min_cost = min(min_cost,costs[0]+min_cost_fun(r1_remains,r2_remains,i+1))
-        r1_remains.pop()
-        print("min_cost",min_cost)
+        r1_remains.pop() # this pop is done because we cannot go ahead with this combination
+        #print("min_cost",min_cost)
 
     if rods[1]>=cut_pieces[i]:
         r2_remains.append(rods[1]-cut_pieces[i])
         min_cost = min(min_cost,costs[1]+min_cost_fun(r1_remains,r2_remains,i+1))
         r2_remains.pop()
-        print("min_cost",min_cost)
-
+        #print("min_cost",min_cost)
 
 
     return min_cost
+
 
     
 
